eb3985578adb01bd6100ec1bac03ff6a80c195be
conwabueze <chinonso1nwabueze@gmail.com>
2021-01-30 10:23:48 -0500

    app alt

--- src/App.js
+++ src/App.js
@@ -10,31 +10,79 @@ import Account from './components/Account/Account';
 import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      newRender: '',
+    };
+
+    this.appRerender = this.appRerender.bind(this);
+  }
   async componentDidMount() {
     const checkUser = await axios.get(
-      'http://localhost:3001/api/v1/users/currentUser'
+      'http://localhost:3001/api/v1/users/loggedInUser',
+      { withCredentials: true }
     );
+    if (checkUser) {
+      this.setState({ user: checkUser.data.data.user });
+    }
+  }
 
-    console.log(checkUser);
+  async componentDidUpdate() {
+    if (this.state.user === '') {
+      const checkUser = await axios.get(
+        'http://localhost:3001/api/v1/users/loggedInUser',
+        { withCredentials: true }
+      );
+
+      if (checkUser) {
+        this.setState({ user: checkUser.data.data.user });
+      }
+    }
+  }
+
+  appRerender() {
+    this.setState({ newRender: '' });
   }
 
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar user={this.state.user} />
+
         <Switch>
           <Route exact path="/" component={Home} />
+
           <Route exact key="air-jordan" path="/sneakers/air-jordan">
             <SneakerDirectory brandDirectory="air-jordan" />
           </Route>
+
           <Route exact key="nike" path="/sneakers/nike">
             <SneakerDirectory brandDirectory="nike" />
           </Route>
+
           <Route exact key="adidas" path="/sneakers/adidas">
             <SneakerDirectory brandDirectory="adidas" />
           </Route>
+
           <Route exact path="/sneakers/:brand/:sneakerID" component={Sneaker} />
-          <Route exact path="/login" component={Account} />
+
+          <Route exact key="login" path="/login">
+            <Account
+              user={this.state.user}
+              appRerender={this.appRerender}
+              renderLogin={true}
+            />
+          </Route>
+
+          <Route exact key="account-settings" path="/account-settings">
+            <Account
+              user={this.state.user}
+              appRerender={this.appRerender}
+              renderSettings={true}
+            />
+          </Route>
         </Switch>
         <Footer />
       </div>
